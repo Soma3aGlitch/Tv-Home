5373dab56243e7dfce55ac2b10a8de55e9d5d3c7
Soma3aGlitch <101549296+Soma3aGlitch@users.noreply.github.com>
2022-08-18 13:14:58 +0200

    Add files via upload

---
+++ work.py
@@ -0,0 +1,37 @@
+from flask import Flask , render_template , url_for , request
+import requests
+app = Flask(__name__)
+title1="batman"
+@app.route("/")
+def main():
+   rawdata= requests.get("https://fake-api-rest-app.herokuapp.com/movies?q="+title1)
+   movies =rawdata.json()
+   return render_template("home.html",movies=movies)
+
+@app.route("/<title>")
+def movies_by_title(title):
+ rawdata= requests.get("https://fake-api-rest-app.herokuapp.com/movies?q="+title)
+ movies =rawdata.json()
+ return render_template("home.html",movies=movies)
+
+
+@app.route("/single_movie/<title>")
+def single_movie(title):
+ rawdata= requests.get("https://fake-api-rest-app.herokuapp.com/movies?title="+title)
+ movies =rawdata.json()
+ return render_template("single_movie.html",movies=movies)
+
+@app.route("/search")
+def search_form():
+ return  render_template("search.html")
+
+@app.route("/search_by_title", methods=["POST"])
+def search_by_title():
+ title =request.form["title"]
+ if title !="":
+  rawdata= requests.get("https://fake-api-rest-app.herokuapp.com/movies?q="+title)
+  movies =rawdata.json()
+ return render_template("search.html",movies=movies)
+
+if __name__=="__main__":
+    app.run(debug=True)
